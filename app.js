ef7eadeefd23b0d5c14ba33a7375b8a3982795f1
fersantisi <fer.santisi@hotmail.com>
2022-11-01 19:51:17 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+let PORT = 3030;
+
+app.listen(PORT, console.log('Listening on port ' + PORT));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve('./views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('./views/login.html'));
+})
